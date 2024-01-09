67f4a84e5f4c1a061511da885cf78a641ed0b301
simonasofronici <120501805+simonasofronici@users.noreply.github.com>
2024-01-09 14:56:15 +0200

    Added last modifications for lab 7.

--- src/main/java/com/example/splaboratoryproject/AlignCenter.java
+++ src/main/java/com/example/splaboratoryproject/AlignCenter.java
@@ -1,6 +1,5 @@
 package com.example.splaboratoryproject;
 public class AlignCenter implements AlignStrategy {
-
     @Override
     public void render(Paragraph p, Content c) {
         System.out.println("Aligning to the center: " + p.getText());
